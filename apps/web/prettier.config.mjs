d8c10c28e75b5983126c07f7a7711613bbf42ccf
조하은 <jhe93@naver.com>
2026-07-01 21:19:52 +0900

    chore(config): Tailwind 클래스 정렬용 Prettier 설정 추가
    - prettier-plugin-tailwindcss 설치
    - apps/web 전용 Prettier 설정 추가
    - lint-staged에서 web 파일에 Prettier config 명시

---
+++ apps/web/prettier.config.mjs
@@ -0,0 +1,8 @@
+import baseConfig from '@repo/prettier-config';
+import * as tailwindPlugin from 'prettier-plugin-tailwindcss';
+
+export default {
+  ...baseConfig,
+  plugins: [tailwindPlugin],
+  tailwindStylesheet: './app/styles/globals.css',
+};
