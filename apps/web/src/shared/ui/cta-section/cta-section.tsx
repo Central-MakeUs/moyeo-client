b8b02b69cec4a4c58ebdc799869c106061c03cc4
조하은 <jhe93@naver.com>
2026-07-18 15:03:29 +0900

    fix: sectionClassName 제거 및 buttonClassName추가

--- apps/web/src/shared/ui/cta-section/cta-section.tsx
+++ apps/web/src/shared/ui/cta-section/cta-section.tsx
@@ -4,18 +4,18 @@ import { cn } from '@/shared/lib/cn';
 import { Button } from '@/shared/ui/button';
 
 type CTASectionProps = Omit<React.ComponentProps<typeof Button>, 'fullWidth'> & {
-  sectionClassName?: string;
+  buttonClassName?: string;
 };
 
 export function CTASection({
   children = '다음',
   className,
-  sectionClassName,
+  buttonClassName,
   ...buttonProps
 }: CTASectionProps) {
   return (
-    <section className={cn('w-full rounded-t-md bg-neutral-0 px-5 pt-5 pb-11', sectionClassName)}>
-      <Button fullWidth className={className} {...buttonProps}>
+    <section className={cn('rounded-t-md w-full bg-neutral-0 px-5 pt-5 pb-11', className)}>
+      <Button fullWidth className={buttonClassName} {...buttonProps}>
         {children}
       </Button>
     </section>
