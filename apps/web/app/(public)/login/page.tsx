f0d5faea2ae48331a474066ee70ec9e00f89d0a2
조하은 <jhe93@naver.com>
2026-07-21 18:57:56 +0900

    init(web): 인증 라우트 최소 골격 추가

---
+++ apps/web/app/(public)/login/page.tsx
@@ -0,0 +1,3 @@
+export default function LoginPage() {
+  return <main>ACC-01 login placeholder</main>;
+}
---
+++ apps/web/app/(public)/onboarding/page.tsx
@@ -0,0 +1,3 @@
+export default function OnboardingPage() {
+  return <main>ONB-01 onboarding placeholder</main>;
+}
